7823b801e4e72fb8f54de07f20bba0f05fdcb223
Vinicius Noronha <viniciusna633@outlook.com>
2022-02-17 12:22:17 -0300

    inclusão de socket na home

--- project/front-end/home/scripts/index.js
+++ project/front-end/home/scripts/index.js
@@ -7,3 +7,19 @@ function closeModal(modalId) {
     let modal = document.getElementById(modalId);
     modal.style.visibility = "hidden";
 }
+
+const url = window.location.href.slice(7,-1);
+
+document.getElementById('play-now-button').addEventListener('click', ()=>{
+   const socket = new WebSocket(`ws://${url}:80/line`);
+
+   socket.onclose((event)=>{
+      const message = JSON.parse(event);
+
+      if(message.data === 1000){
+        //Pedir a página game-board
+        //const socket = new WebSocket(`ws://${url}:80/gamestream`);
+        closeModal('modal-loading');
+      }
+   })
+})
