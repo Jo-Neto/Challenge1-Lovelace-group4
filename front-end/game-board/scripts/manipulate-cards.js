a5d10df97c3b925eb171072b9a0d5653c8a0a0bb
Vinicius Noronha Almeida <91355924+viniciusna@users.noreply.github.com>
2022-02-23 13:00:39 -0300

    Aa (#74)
    * flavia merge
    
    * Fixed front-end
    
    * Fixing management problems
    
    * Merge da flavia e correção de ID
    
    * aaaa
    
    * ax|X|\
    
    * ss
    
    * cursor nas cartas
    
    * Scrr, Deus
    
    * [ADD] Responsivity configuration stuff
    
    * incrementando o tutorial
    
    * Adição no tutorial da home
    
    * a
    
    * tratamento de códigos vindo do server
    
    * correção em modal
    
    Co-authored-by: Jose Neto <jcb.oliveira.neto@gmail.com>
    Co-authored-by: flavia <sflavia051@gmail.com>
    Co-authored-by: Flávia Souza <79340035+flaaaaaavis@users.noreply.github.com>

--- front-end/game-board/scripts/manipulate-cards.js
+++ front-end/game-board/scripts/manipulate-cards.js
@@ -170,7 +170,7 @@ gameSocket.onclose = (event) => {
         console.log("Você trapaceou")
         openModal("modal-cheat")
     } else {
-        openModal("unknow-error")
+        openModal("modal-unknow-error")
     }
 }
 
