2de5161645c65c142f5fe260e5c1fcaa91f96993
flavia <sflavia051@gmail.com>
2022-02-28 15:36:55 -0300

    Created modal background

--- front-end/board-scripts/scripts.js
+++ front-end/board-scripts/scripts.js
@@ -1,9 +1,17 @@
 function openModal(modalId) {
+    const container = document.getElementById('container-modais');
+    container.style.visibility = "visible";
+
+    $('.modal-div').css('visibility', 'hidden');
+
     const modal = document.getElementById(modalId);
     modal.style.visibility = "visible";
 }
 
 function closeModal(modalId) {
+    const container = document.getElementById('container-modais');
+    container.style.visibility = "hidden";
+
     const modal = document.getElementById(modalId);
     modal.style.visibility = "hidden";
 }
