985d7acd6577147c19ade792bd17614483dd3fde
flavia <sflavia051@gmail.com>
2022-03-01 00:57:24 -0300

    Created a button to turn off the tooltips

--- front-end/board-scripts/scripts.js
+++ front-end/board-scripts/scripts.js
@@ -29,6 +29,20 @@ function openMenu(modalId) {
     modal.style.visibility = "visible";
 }
 
+$('#btn-guide').click(function() {
+    if ($(this).attr('src') == './board-assets/subtitles_white_24dp.svg') {
+        $(this).removeAttr('src');
+        $(this).attr('src', './board-assets/subtitles_off_white_24dp.svg');
+
+        $( document ).tooltip({ disabled: true });
+    } else {
+        $(this).removeAttr('src');
+        $(this).attr('src', './board-assets/subtitles_white_24dp.svg');
+
+        $( document ).tooltip({ disabled: false });
+    };
+})
+
 function flipCard() {
     const card = document.querySelector(".flip-card .flip-card-inner");
     card.style.transform = "rotateY(180deg)";
