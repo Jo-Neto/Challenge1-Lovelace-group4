a86761fefe1651ff7e51547f211e7038be17584d
Neto <91321255+Jo-Neto@users.noreply.github.com>
2022-03-21 21:59:32 -0300

    Hotfix (#188)
    * hotfix
    
    * feature 1

--- objects/user/library/register.js
+++ objects/user/library/register.js
@@ -1,10 +1,43 @@
+const fs = require('fs');
+
 const User = require('../user.js');
+
 const storeUsr = require('./db-writer.js');
 
-
 function usrReg(data, res) {
-    //TODO: deny invalid name, mail and pass, and 'Clasher' username
-    storeUsr(new User(data.name, data.email, data.password), true);
+    
+    let db = JSON.parse(fs.readFileSync('./database/users.json'));
+    let lowName = data.name.toLowerCase();
+    let lowEmail = data.email.toLowerCase();
+
+    //TODO: weird character limitation
+    
+    let unavailable = db.some( el => {
+        
+        if (el.name.toLowerCase() === lowName) {
+            res.send("Nickname não disponível");
+            return true;
+        }
+        
+        else if (el.email.toLowerCase() === lowEmail) {
+            res.send("Email não disponível");
+            return true;
+        }
+        
+        else
+            return false;
+
+    });
+
+    if (!unavailable) {
+        storeUsr(new User(data.name, data.email, data.password), true);
+        res.send("Conta registrada, bem vindo "+data.name+" !");
+        return true;
+    }
+    else 
+        return false;
+
+    //TODO: password limitations
 }
 
 module.exports = usrReg;
