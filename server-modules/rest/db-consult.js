baf038267c0b1403eb8a2d7f4adcd0cda2e8d262
Jo-Neto <jcb.oliveira.neto@gmail.com>
2022-03-25 10:20:59 -0300

    replace 1

--- server-modules/rest/db-consult.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-module.exports = function gameConsult(req, res) {
-    fs.readFile('./database/game-sessions.json', (err, readData) => {
-        let db = JSON.parse(readData);
-        res.json(db);
-    });
-};
